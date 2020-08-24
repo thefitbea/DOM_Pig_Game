0d249a32bb0c6a1e717906fbb0a158116a890d95
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2020-08-24 18:41:03 +0530

    fully fledged working game

--- app.js
+++ app.js
@@ -9,52 +9,69 @@ GAME RULES:
 
 */
 
-var globalScore, roundScore, activePerson;
+var globalScore, roundScore, activePerson,gameState;
 
-globalScore = [0, 0];
-roundScore = 0;
-activePerson = 0;
-
-// $(".dice").style.display = "none"; //didnt work
-document.querySelector(".dice").style.display = "none"; // so used DOM
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+gameInitialisation();
 
 document.querySelector(".btn-roll").addEventListener("click", function() { //stil jquery cant be used
-  var dice = Math.floor((Math.random() * 6) + 1); //generates random score for dice whose scope is within this function
-  document.querySelector(".dice").style.display = "block";
+  if(gameState){
+    var dice = Math.floor((Math.random() * 6) + 1); //generates random score for dice whose scope is within this function
+    document.querySelector(".dice").style.display = "block";
 
-  document.querySelector(".dice").setAttribute("src", "dice-" + dice + ".png");
-  //OR - document.querySelector(".dice").src("dice-"+dice+".png");
-  if (dice !== 1) {
-    roundScore += dice; //same as roundScore=roundScore+dice;
-    $("#current-" + activePerson).text(roundScore); //adds score generated randomly
-  } else {
-    nextPlayer();
+    document.querySelector(".dice").setAttribute("src", "dice-" + dice + ".png");
+    //OR - document.querySelector(".dice").src("dice-"+dice+".png");
+    if (dice !== 1) {
+      roundScore += dice; //same as roundScore=roundScore+dice;
+      $("#current-" + activePerson).text(roundScore); //adds score generated randomly
+    } else {
+      nextPlayer();
+    }
   }
-
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  globalScore[activePerson] += roundScore; //OR we could PUSH into array, too you know;
-  document.querySelector("#score-" + activePerson).textContent = globalScore[activePerson];
-  if (globalScore[activePerson] >= 10) {
-    document.querySelector("#name-" + activePerson).textContent = "WINNER!";
-    document.querySelector(".dice").style.display = "none";
-    document.querySelector(".player-" + activePerson + "-panel").classList.add("winner");
-    document.querySelector(".player-" + activePerson + "-panel").classList.remove("active");
-  } else {
-    nextPlayer();
+  if(gameState){
+    globalScore[activePerson] += roundScore; //OR we could PUSH into array, too you know;
+    document.querySelector("#score-" + activePerson).textContent = globalScore[activePerson];
+    if (globalScore[activePerson] >= 10) {
+      document.querySelector("#name-" + activePerson).textContent = "WINNER!";
+      document.querySelector(".dice").style.display = "none";
+      document.querySelector(".player-" + activePerson + "-panel").classList.add("winner");
+      document.querySelector(".player-" + activePerson + "-panel").classList.remove("active");
+      gameState=false;
+    } else {
+      nextPlayer();
+    }
   }
 });
 
+document.querySelector(".btn-new").addEventListener("click", gameInitialisation);
+
+function gameInitialisation(){
+  globalScore = [0, 0];
+  roundScore = 0;
+  activePerson = 0;
+  gameState=true;
+  // $(".dice").style.display = "none"; //didnt work
+  document.querySelector(".dice").style.display = "none"; // so used DOM
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  //resetting values
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");//we removed it to make sure that we don't have two classes being added, then added it back just in case if player 1 was having class "active"
+}
 
 function nextPlayer() {
   activePerson === 0 ? activePerson = 1 : activePerson = 0;
   roundScore = 0; //resetting score to zero
-  document.getElementById("current-0").textContent = "0"; //updating current score too to zero
+  document.getElementById("current-0").textContent = "0"; //updating current score too, to zero
   document.getElementById("current-1").textContent = "0";
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
